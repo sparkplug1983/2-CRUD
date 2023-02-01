b279709a5b74e8018b38f0d5a576742917673897
Fernando <fevaldez.dwfsc8@ucamp.utel.edu.mx>
2023-01-31 22:32:53 -0600

    2nd commit

--- index2.js
+++ index2.js
@@ -1,7 +1,9 @@
 var inp = document.querySelector(".firstName");
-var list = document.querySelector(".studentsList");  
+var list = document.querySelector(".subscribersList");  
 var addData = document.querySelector(".btnSave"); 
 var clearData = document.querySelector(".btnClear"); 
+var deleteData = document.querySelector("btnDelete");
+var editData = document.querySelector("btnEdit");
 var studentsList = [];  
 
 function render(elemments) {
@@ -16,20 +18,48 @@ function render(elemments) {
 
 addData.addEventListener("click", e => {
     if (inp.value !== "") {
-        studentsList.push(inp.value);
+        subscribersList.push(inp.value);
         inp.value = "";
-        render(studentsList);
+        render(subscribersList);
         clearData.style.display = "block";
-        localStorage.setItem("mylist", JSON.stringify(studentsList));        
-    }     else { 
-        alert("Please fill in all fields", "danger");
+        localStorage.setItem("mylist", JSON.stringify(subscribersList));        
+    }     
+    
+    else { 
+        if (subscribersList == null) {
+            const list = document.querySelector("subscribersList");
+            const row = document.createElement("tr");
+            
+            row.innerHTML = `
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>
+            <a href="#" class="btn btn-warning btn-sn edit">Edit</a>
+            <a href="#" class="btn btn-danger btn-sn delete">Delete</a>
+            `;
+            list.appendChild(row);
+            selectedRow = null;
+            showAlert(`New subscriber "${firstName} ${lastName}" has been added`, "success");
+        }
+        else {
+            studentsList.children[0].textContent = firstName;
+            studentsList.children[1].textContent = lastName;
+    
+            studentsList = null;
+            showAlert("Subscriber has been edited", "info");
+        }
+   
     }
 });
 
+
+
+
+
 let saved = localStorage.getItem("mylist");
 if (saved) {
-    studentsList = JSON.parse(localStorage.getItem("mylist"));
-    render(studentsList);
+    subscribersList = JSON.parse(localStorage.getItem("mylist"));
+    render(subscribersList);
 } else {
     clearData.style.display = "none";
 } 
@@ -37,7 +67,28 @@ if (saved) {
 clearData.addEventListener("click", function (){
     localStorage.clear();
     list.innerHTML="";
-    studentsList = [];
+    subscribersList = [];
     clearData.style.display = "none";
-
 });
+
+editData.addEventListener("click",  (e)=>{
+    target = e.target
+localStorage.clear();
+if (target.class.container("btnEdit")){
+    target.parentElement.parentElement.remove();
+}
+} );
+
+
+/*show alert*/
+function showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const main = document.querySelector(".main");
+    container.insertBefore(div, main);
+
+    setTimeout(() => document.querySelector(".alert").remove(), 500);
+}
